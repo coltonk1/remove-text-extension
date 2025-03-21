5a06744398ebb19565796d075831ba3c15e720be
Colton <81594541+coltonk1@users.noreply.github.com>
2025-03-21 16:12:00 -0400

    update to only do interactable elements

--- scripts/remove_text.js
+++ scripts/remove_text.js
@@ -1,9 +1,17 @@
 function hide_text() {
-    const targetElements = document.querySelectorAll("*");
+    const targetElements = document.querySelectorAll("button, a, select, label");
 
     targetElements.forEach((element) => {
         element.style.setProperty("color", "transparent", "important");
     });
+
+    const style = document.createElement("style");
+    style.innerHTML = `
+        input::placeholder, textarea::placeholder {
+            color: transparent !important;
+        }
+    `;
+    document.head.appendChild(style);
 }
 
 hide_text();
